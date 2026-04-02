4377c1fb4a378e259320f4d5f1c00ef998bcb41c
bicyclecardsj <bicyclecard.j@gmail.com>
2026-04-02 14:01:08 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,12 @@
 N, M = map(int, input().split())
 card_list = list(map(int, input().split()))
 
-def find_max():
-    max = 0
-    for card1_index in range(N - 2):
-        for card2_index in range(card1_index + 1, N - 1):
-            for card3_index in range(card2_index + 1, N):
-                sum = card_list[card1_index] + card_list[card2_index] + card_list[card3_index]
-                if sum == M:
-                    return sum
-                if sum < M and sum > max:
-                    max = sum
-    return max
-                
-print(find_max())
+max = 0
+for card1_index in range(N - 2):
+    for card2_index in range(card1_index + 1, N - 1):
+        for card3_index in range(card2_index + 1, N):
+            sum = card_list[card1_index] + card_list[card2_index] + card_list[card3_index]
+            if max < sum <= M:
+                max = sum
+
+print(max)
